3a3f2cbb7347a77a2ece3d1f2ef1d3834f271db7
crua0316 <cresrugi@gmail.com>
2021-02-17 18:58:13 -0500

    Update pep8

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -4,13 +4,17 @@
     to a JSON file for a persistent model
     """
 import json
+
+
 class FileStorage:
     """Class FileStorage, class tos instance a unique
     object storage that will process all saving and loading
     information from the JSON file
     """
     __file_path = "file.json"
+
     __objects = {}
+
     def all(self):
         """Public Instance Method that return the dictionary
         with all the current elements on the private field
@@ -21,6 +25,7 @@ class FileStorage:
             Class-Name.id(key): obj(value)
         """
         return FileStorage.__objects
+
     def new(self, obj):
         """Public Instance Method that sets the dictionary
         objects with the information of a new instance
