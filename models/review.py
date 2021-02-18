8c30fb4dea6d405bdcb4fdc8adf0b71eed44839d
balbinxx <1815@holbertonschool.com>
2021-02-17 19:10:34 -0500

    add more classes_3

--- models/review.py
+++ models/review.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+"""class review
+    """
+from models.base_model import BaseModel
+
+
+class Review(BaseModel):
+    """class Review from BaseModel empty"""
+    place_id = ""
+    user_id = ""
+    text = ""
