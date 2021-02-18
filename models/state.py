faf2c29081c9ebde101e666fdf9828e0cb244a2e
balbinxx <1815@holbertonschool.com>
2021-02-17 19:08:00 -0500

    add more classes_2

--- models/state.py
+++ models/state.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+"""class state
+    """
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    """class state from
+        BaseModel empty"""
+    name = ""
